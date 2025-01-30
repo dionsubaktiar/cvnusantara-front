26ca21aeaaa391c94032975eb6a6cfc0995852b4
dionsubaktiar <104469933+dionsubaktiar@users.noreply.github.com>
2025-01-30 08:32:50 +0700

    change to form input

--- app/components/lockscreen-debug.tsx
+++ app/components/lockscreen-debug.tsx
@@ -8,24 +8,21 @@ interface LockScreenProps {
 const LockScreenDebug = ({ onUnlock }: LockScreenProps) => {
   const [pin, setPin] = useState<string>("");
 
-  const handleUnlock = async () => {
+  const handleUnlock = async (e: React.FormEvent) => {
+    e.preventDefault();
     try {
       await axios.get(
         "https://backend-cv.nusantaratranssentosa.co.id/sanctum/csrf-cookie"
-        // {
-        //   withCredentials: true,
-        // }
       );
       const response = await axios.post(
         "https://backend-cv.nusantaratranssentosa.co.id/api/pin-verify",
         { pin }
-        // { withCredentials: true }
       );
 
-      if (response.data.success == true) {
+      if (response.data.success) {
         localStorage.setItem("authToken", response.data.verification_token);
         localStorage.setItem("role", response.data.role);
-        onUnlock(); // Unlock the screen
+        onUnlock();
       } else {
         alert("Invalid PIN. Please try again.");
       }
@@ -36,21 +33,23 @@ const LockScreenDebug = ({ onUnlock }: LockScreenProps) => {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <div className=" p-4 rounded shadow border-2">
+      <div className="p-4 rounded shadow border-2">
         <h2 className="text-lg font-bold mb-4">Enter PIN to Unlock</h2>
-        <input
-          type="password"
-          className="border p-2 mb-4 rounded w-full text-black"
-          placeholder="Enter PIN"
-          value={pin}
-          onChange={(e) => setPin(e.target.value)}
-        />
-        <button
-          className="bg-blue-500 text-white px-4 py-2 rounded"
-          onClick={handleUnlock}
-        >
-          Unlock
-        </button>
+        <form onSubmit={handleUnlock}>
+          <input
+            type="password"
+            className="border p-2 mb-4 rounded w-full text-black"
+            placeholder="Enter PIN"
+            value={pin}
+            onChange={(e) => setPin(e.target.value)}
+          />
+          <button
+            type="submit"
+            className="bg-blue-500 text-white px-4 py-2 rounded w-full"
+          >
+            Unlock
+          </button>
+        </form>
       </div>
     </div>
   );
